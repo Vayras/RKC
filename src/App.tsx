ec8a98055b542b7fa8a333f80ab9159757bd84ac
Vayras <89837102+Vayras@users.noreply.github.com>
2024-09-02 13:07:58 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'rsuite/dist/rsuite.min.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
